b0f22fb26c92f128e7219afe07e2c2bb5ef2b32f
Mouloud Mecheter <mouloudmecheter@gmail.com>
2023-07-23 19:01:08 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat) {
+
+    cats.push(cat);
+
+}
+
+function destructivelyPrependCat(cat) {
+    cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(cat) {
+    let newCat = [...cats, cat];
+    return newCat;
+}
+
+
+function prependCat(cat) {
+    let newCat = [cat, ...cats];
+    return newCat;
+}
+function removeLastCat() {
+    let newCat = cats.slice(0, 2);
+    return newCat;
+}
+function removeFirstCat() {
+    let newCat = cats.slice(-2);
+    return newCat;
+}
+
+
